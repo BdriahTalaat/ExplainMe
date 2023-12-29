23eb1d7a456461ac97886ea3cb6692ccc5988948
BdriahFakhrji <bdriahtalaat@gmail.com>
2023-12-29 22:59:13 +0300

    Initial Commit

---
+++ ExplainMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExplainMe
+//
+//  Created by Bdriah Talaat on 16/06/1445 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
